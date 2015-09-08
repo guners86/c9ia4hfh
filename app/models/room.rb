532d1e7fea51ea5fef31e4c1787356d6eb8adce1
guners86 <guners86@hotmail.com>
2015-09-07 19:54:40 -0500

    Se agraga Validadores en el modelo room

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,11 @@
 #
 
 class Room < ActiveRecord::Base
+	validates :title, :presence => {:message => "El campo Titulo es obligatorio"}
+	validates :description, :presence => {:message => "El campo Descripción es obligatorio"}, length: {maximum: 400, :message => "El campo Descripción no debe superar los 400 caracteres "}
+	validates :beds, :presence => {:message => "El campo Número de Camas es obligatorio"}, :numericality => {:only_integer => true, :message => "El campo Número de Camas debe ser Númerico"}
+	validates :guests, :presence => {:message => "El campo Número de Huespedes es obligatorio"}, :numericality => {:only_integer => true, :message => "El campo Número de Huespedes debe ser Númerico"}
+	#validates :created_at, presence: true
+	#validates :updated_at, presence: true
+	validates :image_url, :presence => {:message => "El campo URL de la Imagen es obligatorio"}
 end
